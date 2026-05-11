0e9ce94ac1632ef49d408df5a9d2b605b7127f43
imran sheikh <imranshk30@gmail.com>
2026-05-11 09:57:35 -0400

    validation with regular expressions

--- WebApplication_MVC_CRUD/Models/Student.cs
+++ WebApplication_MVC_CRUD/Models/Student.cs
@@ -7,19 +7,24 @@ namespace WebApplication_MVC_CRUD.Models
         
             public int ID { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Only letters and spaces are allowed.")]
         [StringLength(50, MinimumLength = 3)]
         public string? studentName { get; set; }
         [Required]
         [StringLength(10)]
+        [RegularExpression(@"^(Male|Female|Other)$", ErrorMessage = "Gender must be Male, Female, or Other.")]
         public string? studentGender { get; set; }
         [Required]
         [Range(18,60)]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Age must be a valid number.")]
         public int age { get; set; }
         [Required]
         [StringLength(2)]
+            [RegularExpression(@"^[1-9]$|^10$", ErrorMessage = "Standard must be a number between 1 and 10.")]
         public string? standard { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 3)]
+         [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Only letters and spaces are allowed.")]
         public string? fatherName { get; set; }
         }
 
